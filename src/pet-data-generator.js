155481639fe4d1cf2dcdc6f283679568c88416b4
maxwellbenton <maxwellbenton@gmail.com>
2019-01-14 11:05:15 -0500

    formats js file

--- src/pet-data-generator.js
+++ src/pet-data-generator.js
@@ -47,41 +47,55 @@ const FEMALE_PET_NAMES = [
   "Shy",
   "Ham",
   "Feta",
-  "Hennessy", "Flurry", "Babs", "Miles", "Silent", "Brook", "Maia", "Dori", "Vanilla", "Zelda", "Peekaboo", "Elsa", "Cherry", "Silent", "Mickey"
+  "Hennessy",
+  "Flurry",
+  "Babs",
+  "Miles",
+  "Silent",
+  "Brook",
+  "Maia",
+  "Dori",
+  "Vanilla",
+  "Zelda",
+  "Peekaboo",
+  "Elsa",
+  "Cherry",
+  "Silent",
+  "Mickey"
 ];
 
-const ANIMAL_TYPES = ['cat', 'cat', 'cat', 'cat', 'dog', 'dog', 'dog', 'dog', 'micropig'];
+const ANIMAL_TYPES = [ 'cat', 'cat', 'cat', 'cat', 'dog', 'dog', 'dog', 'dog', 'micropig' ];
 
-const uuid = require('node-uuid');
+const uuid = require( 'node-uuid' );
 
 function getRandomAge() {
-  return Math.ceil(Math.random() * 10);
+  return Math.ceil( Math.random() * 10 );
 }
 
 function getRandomWeight() {
-  return Math.ceil(Math.random() * 6);
+  return Math.ceil( Math.random() * 6 );
 }
 
-function getRandomFromArray(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
+function getRandomFromArray( arr ) {
+  return arr[ Math.floor( Math.random() * arr.length ) ];
 }
 
 const AMOUNT_OF_PETS = 35;
 
 const result = [];
 
-for (let i = 0; i < AMOUNT_OF_PETS; i++) {
+for ( let i = 0; i < AMOUNT_OF_PETS; i++ ) {
   const pet = {
     id: uuid.v4(),
-    type: getRandomFromArray(ANIMAL_TYPES),
-    gender: getRandomFromArray(['male', 'female']),
+    type: getRandomFromArray( ANIMAL_TYPES ),
+    gender: getRandomFromArray( [ 'male', 'female' ] ),
     age: getRandomAge(),
     weight: getRandomWeight(),
   };
 
-  pet.name = pet.gender === 'male' ? getRandomFromArray(MALE_PET_NAMES) : getRandomFromArray(FEMALE_PET_NAMES);
+  pet.name = pet.gender === 'male' ? getRandomFromArray( MALE_PET_NAMES ) : getRandomFromArray( FEMALE_PET_NAMES );
 
-  result.push(pet);
+  result.push( pet );
 }
 
-console.log(JSON.stringify(result, null, 2));
+console.log( JSON.stringify( result, null, 2 ) );
